00db1016e7ce2739eac8b6beead546689c4faf4b
Virginie Genes <virginiegenes@hotmail.com>
2024-10-21 11:17:25 -0300

    new code

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,24 @@
 # 1. Pedir o primeiro número
-
+puts "Please enter the first number"
+first_number = gets.chomp.to_i
 # 2. Pedir o segundo número
-
+puts "Please enter the second number"
+second_number = gets.chomp.to_i
 # 3. Pedir qual a operação [+ - / *]
-
+puts "Which operation do you want to perform ?"
+operation = gets.chomp
 # 4. realizar a operação
+case operation
+when "+"
+  result = first_number + second_number
+when "-"
+  result = first_number - second_number
+when "/"
+  result = first_number / second_number
+when "*"
+  result = first_number * second_number
+else
+  result = "invalid"
+end
 
-# 5. exibir o resultado
+puts "The result is #{result}"
