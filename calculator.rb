32337bc1c11e3ed85382de375a05f47fb4a116b4
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-21 11:27:26 -0300

    refactor

--- calculator.rb
+++ calculator.rb
@@ -7,18 +7,16 @@ second_number = gets.chomp.to_i
 # 3. Pedir qual a operação [+ - / *]
 puts "Which operation do you want to perform ?"
 operation = gets.chomp
+
 # 4. realizar a operação
-case operation
-when "+"
-  result = first_number + second_number
-when "-"
-  result = first_number - second_number
-when "/"
-  result = first_number / second_number
-when "*"
-  result = first_number * second_number
+result = case operation
+when "+" then first_number + second_number
+when "-" then first_number - second_number
+when "/" then first_number / second_number
+when "*" then first_number * second_number
 else
-  result = "invalid"
+  "invalid"
 end
 
+# Exibir o resultado
 puts "The result is #{result}"
